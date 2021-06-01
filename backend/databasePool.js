b29e40977b4b0fac0dc9d6ffa59ea22795f9392f
Pitson3 <Josiahprojects1@gmail.com>
2021-06-01 08:29:49 +0200

    Made the databasePool.js file shareable

--- backend/databasePool.js
+++ backend/databasePool.js
@@ -3,3 +3,5 @@ const { Pool } = require("pg");
 const databaseConfiguration = require("./secrets/databaseConfiguration.js")
 
 const pool = new Pool(databaseConfiguration);
+
+module.exports = pool;
