33d146f7e1f765e72d93299f6af1b891ae444669
Pitson3 <Josiahprojects1@gmail.com>
2021-06-01 08:47:41 +0200

    Added the test code for the Pool query and commented it in the databasePool.js file

--- backend/databasePool.js
+++ backend/databasePool.js
@@ -5,3 +5,12 @@ const databaseConfiguration = require("./secrets/databaseConfiguration.js")
 const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
+
+/** 
+pool.query("SELECT * FROM generation ", (error, response) => {
+    if(error) console.log("error", error);
+
+    console.log("response.rows", response.rows);
+})
+
+**/
